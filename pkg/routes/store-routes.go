be4b9c56552614f77506037a9958d42645057b8a
DJ Phoenix <phoenix@Dikes-MacBook-Air.local>
2024-10-21 22:36:17 +0300

    Fixed route urls

--- pkg/routes/store-routes.go
+++ pkg/routes/store-routes.go
@@ -8,7 +8,7 @@ import (
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetAllBooks).Methods("GET")
-	router.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
